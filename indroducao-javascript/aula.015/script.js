d67b0dce07a31fb7697860dfb35c0abd95c6357e
lucassouzadelima81-cloud <lucassouzadelima81@gmail.com>
2026-04-22 21:21:45 -0300

    Add files via upload

---
+++ indroducao-javascript/aula.015/script.js
@@ -0,0 +1,9 @@
+let filmesTop10 = ["barbie", "sonic", "super Mario", "homemAranha", "batman", "mulherMaravilha", "capitaoAmerica",
+    "homemDeFerro", "thor", "hulk"]
+
+    let array = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9, 10]
+    ]
+    console.log(array[0][0])
